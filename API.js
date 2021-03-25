ad01db551c3a0d67201e10f0520637128f3e70cc
Anhydrite <robinzmuda@gmail.com>
2021-03-25 13:19:36 +0100

    not my first commit

---
+++ API.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+
+app.listen(8080, () => {
+    try{
+        fs.readFileSync("clapCount.txt")
+    }catch(e){
+        fs.writeFileSync("clapCount.txt", "0", (err)=>{
+            res.send(err);
+        })
+    }
+    console.log('Serveur à l\'écoute')
+})
+  
+app.get('/clap', (req,res) => {
+    let content = "";
+    content = fs.readFileSync("clapCount.txt", "utf8", (err)=>{
+        if(err){
+            console.log(err);
+            throw err;
+        }
+    })
+    content = (parseInt(content)+1).toString();
+    console.log(content);
+    fs.writeFileSync("clapCount.txt", content.toString(), (err)=>{
+        res.send(err);
+    })
+    res.send(content);
+})
